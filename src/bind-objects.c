62c2496a93ebe0f4ea86bb13db3d3046d405e075
wangying <wy721@qq.com>
2017-04-11 12:35:43 +0800

    V 170411.1

--- src/bind-objects.c
+++ src/bind-objects.c
@@ -12,7 +12,7 @@
 #include "baye/comm.h"
 #include "baye/consdef.h"
 
-static U8 g_engineVersion[32] = "170410";
+static U8 g_engineVersion[32] = "170411.1";
 
 void bind_skill_var(ObjectDef* def);
 void bind_show_face(ObjectDef* def);
